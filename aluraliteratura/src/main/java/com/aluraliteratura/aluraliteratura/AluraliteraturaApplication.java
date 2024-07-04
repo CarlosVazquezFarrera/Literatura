83d936c71bf609652cec3d01700b10d627c43317
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2024-07-03 19:33:37 -0600

    Initial

---
+++ aluraliteratura/src/main/java/com/aluraliteratura/aluraliteratura/AluraliteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.aluraliteratura.aluraliteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraliteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraliteraturaApplication.class, args);
+	}
+
+}
---
+++ aluraliteratura/src/test/java/com/aluraliteratura/aluraliteratura/AluraliteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluraliteratura.aluraliteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraliteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
